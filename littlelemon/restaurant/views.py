556766ae2453c6b4b59d158b06d2b2aa4fafbec8
Ahmed Maher Salam <ahmedmahersalam22@gmail.com>
2023-04-23 11:46:19 +0200

    API

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,7 +1,30 @@
 from django.shortcuts import render
+# from .serializers import MenuSerializer
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .serializers import MenuSerializer, BookingSerializer
+from .models import Booking, Menu
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+
+class MenuItemsView(ListCreateAPIView):
+    serializer_class = MenuSerializer
+    queryset = Menu.objects.all()
+
+
+class SingleMenuItemView(RetrieveUpdateDestroyAPIView):
+    serializer_class = MenuSerializer
+    queryset = Menu.objects.all()
+
+
+class BookingViewSet(ModelViewSet):
+    serializer_class = BookingSerializer
+    queryset = Booking.objects.all()
+
+    
+
+
 
     
